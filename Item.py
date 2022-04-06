b47faa88b75c90e7039cd7b909ddf2db8be35545
SlamminSpeedemon <harshyboi2004@gmail.com>
2022-04-06 09:51:39 -0500

    Updated Item Object to handle all object related GUI elements

--- Item.py
+++ Item.py
@@ -1,20 +1,30 @@
 from tkinter import *
 class Item:
-    def __init__(self, itemCost, itemName):
+    def __init__(self, itemCost, itemName, rootWindow):
         self.itemCost = itemCost
         self.itemCount = 0
-        self.itemString = StringVar()
         self.itemName = itemName
+        self.root = rootWindow
+        self.itemString = StringVar()
+        self.itemEntry = Entry(rootWindow)
 
-    def addDog(self, additionalDogs):
-        self.itemCount += additionalDogs
+    def itemAdd(self):
+        self.itemCount += 1
 
-    def setDog(self, setDog):
-        self.itemCount = setDog
 
-    def guiStart(self, root, xPos, yPos):
+    def addItem(self, addItems):
+        self.itemCount += addItems
+
+    def setItemCount(self, setItemCount):
+        self.itemCount = setItemCount
+
+    def guiStart(self, xPos, yPos):
         self.itemString.set(str(self.itemCount) + " " + self.itemName + " (" + str(self.itemCost) + "0)")
-        Label(root, textvariable=self.itemString, bg='#FFEFDB', font=('arial', 18, 'normal')).place(x=xPos, y=yPos)
+        Label(self.root, textvariable=self.itemString, bg='#FFEFDB', font=('arial', 18, 'normal')).place(x=xPos, y=yPos)
+
+        self.itemEntry.place(x=xPos + 230, y=yPos + 5)
+
+        Button(self.root, text=("+ " + self.itemName), bg='#8B8378', font=('arial', 18, 'normal'), command=self.itemAdd()).place(x=xPos + 370, y=yPos-10)
 
     def guiUpdate(self):
         self.itemString.set(str(self.itemCount) + " " + self.itemName + " (" + str(self.itemCost) + "0)")
--- main.py
+++ main.py
@@ -145,82 +145,29 @@ labelsYPos = 40
 labelYposChanger = 70
 
 #set up objects to handle GUI
-hotdogObject = Item(hotdogCost, "hotdogs")
-hotdogObject.guiStart(root, labelsXpos, labelsYPos)
+hotdogObject = Item(hotdogCost, "hotdogs", root)
+hotdogObject.guiStart(labelsXpos, labelsYPos)
 
-bratObject = Item(bratCost, "brats")
-bratObject.guiStart(root, labelsXpos, labelsYPos + labelYposChanger)
+bratObject = Item(bratCost, "brats", root)
+bratObject.guiStart(labelsXpos, labelsYPos + labelYposChanger)
 
-hamburgerObject = Item(hamburgerCost, "hamburgers")
-hamburgerObject.guiStart(root, labelsXpos, labelsYPos + 2 * labelYposChanger)
+hamburgerObject = Item(hamburgerCost, "hamburgers", root)
+hamburgerObject.guiStart(labelsXpos, labelsYPos + 2 * labelYposChanger)
 
-friesObject = Item(sodaCost, "fries")
-friesObject.guiStart(root, labelsXpos, labelsYPos + 3 * labelYposChanger)
+friesObject = Item(sodaCost, "fries", root)
+friesObject.guiStart(labelsXpos, labelsYPos + 3 * labelYposChanger)
 
-sodasObject = Item(sodaCost, "sodas")
-sodasObject.guiStart(root, labelsXpos, labelsYPos + 4 * labelYposChanger)
+sodasObject = Item(sodaCost, "sodas", root)
+sodasObject.guiStart(labelsXpos, labelsYPos + 4 * labelYposChanger)
 
-watersObject = Item(0, "waters")
-watersObject.guiStart(root, labelsXpos, labelsYPos + 5 * labelYposChanger)
-
-
-
-# This is the section of code which creates a text input box
-dogInput=Entry(root)
-dogInput.place(x=217, y=43)
-
-
-# This is the section of code which creates a text input box
-bratInput=Entry(root)
-bratInput.place(x=217, y=113)
-
-
-# This is the section of code which creates a text input box
-hamburgerInput=Entry(root)
-hamburgerInput.place(x=217, y=183)
-
-
-# This is the section of code which creates a text input box
-friesInput=Entry(root)
-friesInput.place(x=217, y=253)
-
-
-# This is the section of code which creates a text input box
-sodaInput=Entry(root)
-sodaInput.place(x=217, y=323)
-
-
-# This is the section of code which creates a text input box
-waterInput=Entry(root)
-waterInput.place(x=217, y=403)
+watersObject = Item(0, "waters", root)
+watersObject.guiStart(labelsXpos, labelsYPos + 5 * labelYposChanger)
 
 
 # This is the section of code which creates a button
 Button(root, text='Update', bg='#8B8378', font=('arial', 18, 'normal'), command=btnClickFunction).place(x=227, y=443)
 
 
-# This is the section of code which creates a button
-Button(root, text='+ hotdog', bg='#8B8378', font=('arial', 18, 'normal'), command=dogAdd).place(x=377, y=23)
-
-
-# This is the section of code which creates a button
-Button(root, text='+ brat', bg='#8B8378', font=('arial', 18, 'normal'), command=btnClickFunction).place(x=377, y=93)
-
-
-# This is the section of code which creates a button
-Button(root, text='+ hamburger', bg='#8B8378', font=('arial', 18, 'normal'), command=btnClickFunction).place(x=377, y=163)
-
-
-# This is the section of code which creates a button
-Button(root, text='+ fries', bg='#8B8378', font=('arial', 18, 'normal'), command=btnClickFunction).place(x=377, y=233)
-
-
-# This is the section of code which creates a button
-Button(root, text='+ soda', bg='#8B8378', font=('arial', 18, 'normal'), command=btnClickFunction).place(x=377, y=303)
-
-
-# This is the section of code which creates a button
-Button(root, text='+ water', bg='#8B8378', font=('arial', 18, 'normal'), command=btnClickFunction).place(x=377, y=373)
 
 
 # This is the section of code which creates the a label
